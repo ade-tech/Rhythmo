fae0c4cc6aab857d4e7bf742b64c6c93e49baa30
ade-tech <adelopoadekunle@gmail.com>
2025-03-22 17:50:03 +0100

    Music sides completed

--- src/features/tracks/SongContainer.tsx
+++ src/features/tracks/SongContainer.tsx
@@ -1,5 +1,6 @@
-import { HStack, Image, Spacer, Stack, Text } from "@chakra-ui/react";
-import { HiX } from "react-icons/hi";
+import { Flex, HStack, Image, Spacer, Stack, Text } from "@chakra-ui/react";
+import { GoPlusCircle } from "react-icons/go";
+import { HiOutlineUpload, HiX } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 export function SongContainer() {
@@ -9,26 +10,67 @@ export function SongContainer() {
         <HStack mb={1}>
           <Link to="sada">
             <Text
-              textStyle={"2xl"}
+              textStyle={"lg"}
               color={"white"}
+              transitionDuration={"200ms"}
               _hover={{
                 textDecoration: "underline",
+                transition: "ease-in-out",
               }}
-              fontWeight={"bold"}
+              fontWeight={"medium"}
             >
               SPRINTER
             </Text>
           </Link>
           <Spacer />
-          <HiX className="text-gray-400" size={25} />
+          <HiX className="text-gray-400" size={20} />
         </HStack>
         <Image
-          rounded={"md"}
+          rounded={"lg"}
           width={"300px"}
           height={"300px"}
           objectFit={"cover"}
+          mb={2}
           src="https://swjwzsoqbpfsivdzudfx.supabase.co/storage/v1/object/public/Temp//ab6761610000e5ebf6469f2cbf0a7e78744a3173.jpg"
         />
+        <Stack gap={0}>
+          <Flex>
+            <Stack gap={0}>
+              <Link to="sada">
+                <Text
+                  textStyle={"2xl"}
+                  color={"white"}
+                  transitionDuration={"200ms"}
+                  _hover={{
+                    textDecoration: "underline",
+                    transition: "ease-in-out",
+                  }}
+                  fontWeight={"bold"}
+                >
+                  SPRINTER
+                </Text>
+              </Link>
+              <Text
+                textStyle={"md"}
+                color={"gray.400"}
+                transitionDuration={"200ms"}
+                _hover={{
+                  textDecoration: "underline",
+                  transition: "ease-in-out",
+                }}
+                fontWeight={"medium"}
+              >
+                Central Cee
+              </Text>
+            </Stack>
+            <Spacer />
+            <HStack mr={1}>
+              <Link to="sada"></Link>
+              <HiOutlineUpload size={20} className="text-gray-400" />
+              <GoPlusCircle size={20} className="text-gray-400" />
+            </HStack>
+          </Flex>
+        </Stack>
       </Stack>
     </Stack>
   );
